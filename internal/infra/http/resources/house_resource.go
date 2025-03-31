5ac387fdb683c70ed32a9d81afb13e481e58bc23
BohdanBoriak <lightraven72@gmail.com>
2025-03-31 09:53:45 +0300

    Add: HouseController Save

---
+++ internal/infra/http/resources/house_resource.go
@@ -0,0 +1,37 @@
+package resources
+
+import (
+	"time"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+)
+
+type HouseDto struct {
+	Id          uint64     `json:"id"`
+	UserId      uint64     `json:"userId"`
+	Name        string     `json:"name"`
+	Description *string    `json:"description,omitempty"`
+	City        string     `json:"city"`
+	Address     string     `json:"address"`
+	Lat         float64    `json:"lat"`
+	Lon         float64    `json:"lon"`
+	CreatedDate time.Time  `json:"createdDate"`
+	UpdatedDate time.Time  `json:"updatedDate"`
+	DeletedDate *time.Time `json:"deletedDate,omitempty"`
+}
+
+func (d HouseDto) DomainToDto(h domain.House) HouseDto {
+	return HouseDto{
+		Id:          h.Id,
+		UserId:      h.UserId,
+		Name:        h.Name,
+		Description: h.Description,
+		City:        h.City,
+		Address:     h.Address,
+		Lat:         h.Lat,
+		Lon:         h.Lon,
+		CreatedDate: h.CreatedDate,
+		UpdatedDate: h.UpdatedDate,
+		DeletedDate: h.DeletedDate,
+	}
+}
